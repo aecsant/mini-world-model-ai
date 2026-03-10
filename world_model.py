9a922ddf357a94b6d5c6861ef2603581f0adaa9e
Santosh Kumar <43191852+aecsant@users.noreply.github.com>
2026-03-10 23:51:27 +0530

    Create world_model.py

---
+++ world_model.py
@@ -0,0 +1,10 @@
+class WorldModel:
+    def __init__(self):
+        print("World Model Initialized")
+
+    def run(self, camera_data, sim_env):
+        print("Processing Input Data")
+        print("Running Simulation")
+        print("Predicting Future States")
+        print("Planning Actions")
+        print("Learning from Feedback")
